6f1bcf51b7700dc30e97ba6a798092e4b8908ea0
Akshat Garg <akshatg805@gmail.com>
2023-04-22 20:06:16 +0530

    Update refresh.js
    removed showResult() function as that was only for testing
    Also, this is obsolete now

--- javascript/refresh.js
+++ javascript/refresh.js
@@ -1,9 +1,3 @@
 function refresh_this_page() {
   location.reload();
 }
-
-function showResult() {
-  const resultElement = document.getElementById("result");
-  const result = 2 + 2;
-  resultElement.value = result;
-}
